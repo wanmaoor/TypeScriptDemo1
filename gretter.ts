525ff4e2e921af04991d11ce2f7d7c1c88d5757c
wanmaoor <wanmao1284879375@outlook.com>
2019-07-04 09:03:59 +0800

    Never Object Assertion

--- gretter.ts
+++ gretter.ts
@@ -19,7 +19,7 @@ class Student {
 }
 
 // value type
-let student = new Student('lebron','Jr.','James')
+let student = new Student('lebron', 'Jr.', 'James')
 console.log(gretter(student));
 
 let isDone: boolean = false
@@ -28,26 +28,26 @@ console.log("TCL: isDone:", isDone)
 let decimal: number = 6
 console.log("TCL: decimal:", decimal)
 
-let numberArr: Array<number> = [1,2,5]
+let numberArr: Array<number> = [1, 2, 5]
 console.log("TCL: numberArr", numberArr)
 
-let stringArr: Array<string> = ['a','b','c']
+let stringArr: Array<string> = ['a', 'b', 'c']
 console.log("TCL: stringArr", stringArr)
 
-let booleanArr: boolean[] = [false, true, false ]
+let booleanArr: boolean[] = [false, true, false]
 console.log("TCL: booleanArr", booleanArr)
 
 // Tuple
-let tuple: [string, number] = [ 'a', 123]
+let tuple: [string, number] = ['a', 123]
 console.log("TCL: tuple", tuple)
 
 // Enum
-enum Color {Red, Black, Green}
+enum Color { Red, Black, Green }
 let c: Color = Color.Black
 console.log("TCL: c", c)
 
 // change enum value
-enum newColor {Yellow = 44, Blue = 33}
+enum newColor { Yellow = 44, Blue = 33 }
 let y: newColor = newColor.Yellow
 let b: newColor = newColor.Blue
 console.log("TCL: y", y)
@@ -73,3 +73,25 @@ let voidUndefined: void = undefined
 
 let u: undefined = undefined;
 let n: null = null;
+
+// Never: used when you sure that something is never going to occur or return from a function
+function throwError(errorMsg: string): never {
+  throw new Error(errorMsg);
+}
+
+function keepProcessing(): never {
+  while (true) {
+    console.log('I always does something and never ends.')
+  }
+}
+
+// Object
+// declare function create(o: object | null): void;
+// create({o: 1})
+// create(null)
+
+// Type assertion: I know what I'm doing
+let someValue: any = 'this is a string'
+let someLength: number = (someValue as string).length
+
+console.log("TCL: someLength", someLength)
